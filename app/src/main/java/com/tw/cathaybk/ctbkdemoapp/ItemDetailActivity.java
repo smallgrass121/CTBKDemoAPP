79401606e4a4fb739c3f7d8a7a1d8a1c8f5d2207
smallgrass121 <smallgrass121@gmail.com>
2021-03-02 23:44:21 +0800

    CREATE DEFAULT CTBANK DEMO APP

--- app/src/main/java/com/tw/cathaybk/ctbkdemoapp/ItemDetailActivity.java
+++ app/src/main/java/com/tw/cathaybk/ctbkdemoapp/ItemDetailActivity.java
@@ -29,7 +29,7 @@ public class ItemDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_item_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
-
+//
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
